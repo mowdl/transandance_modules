5f1250be7769e901efe254e0f1b15199bcdefb7f
Mouad El Mekaoui <mouad.elmekaoui@gmail.com>
2025-04-17 17:28:49 +0100

    Add react-icons package and update Footer component with social media links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { SiGithub, SiLinkedin, SiX } from "react-icons/si";
 
 const Footer: React.FC = () => {
     return (
-        <footer className="text-center text-gray-500 text-sm mt-8">
-            Based on ft_transcendence subject document v16.0
+        <footer className="flex flex-col items-center justify-center py-6 bg-gray-50 mt-10">
+            <p className="text-sm text-gray-500  mb-4">
+                Made by mowdl
+            </p>
+            <div className="flex space-x-6">
+                <a
+                    href="https://github.com/mowdl"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-600 hover:text-gray-900 transition-colors"
+                >
+                    <SiGithub size={24} />
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/mouad-el-mekaoui-934bb6207/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-600 hover:text-gray-900 transition-colors"
+                >
+                    <SiLinkedin size={24} />
+                </a>
+                <a
+                    href="https://x.com/mowdl_"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-600 hover:text-gray-900 transition-colors"
+                >
+                    <SiX size={24} />
+                </a>
+            </div>
         </footer>
     );
 };
